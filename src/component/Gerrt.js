fbb2186a53d8cdd46c06f89c674719794906768c
Dinesh1629 <dineshpraba1629@gmail.com>
2020-12-08 13:18:11 +0530

    1 file modified

--- src/component/Gerrt.js
+++ src/component/Gerrt.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-function Gerrt() {
-  return <h1>hello world</h1>;
-}
-export default Gerrt;
+//function Gerrt() {
+  //return <h1>hello world</h1>;
+//}
+const Great = () => <h1>Hello Word</h1>
+//export default Gerrt;
