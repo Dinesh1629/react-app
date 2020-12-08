8319ab3d6f529cbbcbdb05b7eef3224cbd2fe827
Dinesh1629 <dineshpraba1629@gmail.com>
2020-12-08 14:30:19 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 //import gerrt,{compoent} from './compnoent/gerrt'
 import './style.css';
 import Gerrt from './component/Gerrt' 
-
+import Welcome from './component/welcome'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Gerrt />
+        <Welcome />
       </div>
     );
   }
--- src/component/Gerrt.js
+++ src/component/Gerrt.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-//function Gerrt() {
-  //return <h1>hello world</h1>;
-//}
+function Gerrt() {
+  return <h1>hello world</h1>;
+}
 const Great = () => <h1>Hello Word</h1>
-//export default Gerrt;
+export default Gerrt;
